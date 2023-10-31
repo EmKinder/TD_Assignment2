8b94b4f5b6e4912ae3108b5ed8d66339ad81595e
EmKinder <emily.l.kinder@student.uts.edu.au>
2023-10-31 11:47:47 +1100

    wip + publishing functions, name UI and functionality

--- Source/AssetPublishing.py
+++ Source/AssetPublishing.py
@@ -3,7 +3,10 @@ import os
 
 directoryField = None
 directory = ""
+WIP_path = ""
+publish_path = ""
 valid_directory_path = False
+name = ""
 
 def setDirectory(*args):
     global directoryField 
@@ -18,8 +21,31 @@ def setDirectory(*args):
             valid_directory_path = False
         else:
             cmds.textField(directoryField, edit=True, text=directory)
+            WIP_path = directory + "/WIP"
+            publish_path = directory + "/Published"
             print("Selected Directory:", directory)
             valid_directory_path = True
+            
+def updateName(*args):
+    global name
+    name = cmds.textField('nameText', query=True, text=True)
+    print("Sequence Name entered:", sequence_name)
+
+def WIPExport(*args):
+    updateName()
+    global name 
+    global WIP_path
+    global valid_directory_path
+    models = cmds.ls(geometry=True)
+    set_pieces = cmds.ls(type='objectSet')
+    animations = cmds.keyframe(q=True)
+    lighting = cmds.ls(type='light')
+    if(valid_directory_path):
+        cmds.file(file_path, force=True, typ="mayaBinary", exportSelected=True)
+    print("WIP export")
+
+def publishExport(*args):
+    print("Publish export")
 
 def assetPublishing():
     global directoryField 
@@ -27,15 +53,25 @@ def assetPublishing():
     if cmds.window(window_name, exists=True):
         cmds.deleteUI('assetPublishing')
     
-    cmds.window(window_name, title="Asset Publishing", widthHeight=(500, 120))
+    cmds.window(window_name, title="Asset Publishing", widthHeight=(500, 200))
     
     main_layout = cmds.columnLayout(adjustableColumn=True)
     
     directoryField = cmds.textField('directoryField', w=300, placeholderText="Select Directory Path")
 
     cmds.button(label="Set Base Directory", command=setDirectory)
-    cmds.button(label="WIP")
-    cmds.button(label="Publish")  
+    
+    cmds.separator(h=10, style='none')
+    
+    cmds.text('Name', align="center", font="boldLabelFont")
+    cmds.separator(h=5, style='none')
+    text_field = cmds.textField('nameText', placeholderText="Name")
+    
+    cmds.separator(h=10, style='none')
+            
+    cmds.button(label="WIP", command=WIPExport)
+    cmds.separator(h=5, style='none')
+    cmds.button(label="Publish", command=publishExport)  
     
     cmds.showWindow('assetPublishing')
     
