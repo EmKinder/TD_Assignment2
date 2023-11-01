d22ee74d1a695c95a2e457dfe29dec99200161f2
EmKinder <emily.l.kinder@student.uts.edu.au>
2023-11-01 22:48:48 +1100

    set and setpiece publishing functioning

--- Source/AssetPublishing.py
+++ Source/AssetPublishing.py
@@ -2,7 +2,7 @@ import maya.cmds as cmds
 import os
 import platform
 import shutil
-
+import re
 
 current_platform = platform.system()
 
@@ -51,7 +51,7 @@ def WIPExport(*args):
     global name
     global WIP_path
     global valid_directory_path
-    assets = cmds.ls(type="transform", selection=True)[0]
+    #assets = cmds.ls(type="transform", selection=True)[0]
     version_no = 1
     this_directory_name = WIP_path + "/" + name
     print("WIP_path:", WIP_path)
@@ -59,7 +59,7 @@ def WIPExport(*args):
 
     if not os.path.exists(this_directory_name):
         os.makedirs(this_directory_name)
-    file_path = name + "_" + "{:03d}".format(version_no) + ".abc"
+    file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
     full_file_path = os.path.join(this_directory_name, file_path)  # Construct the full file path
 
     print("full_file_path:", full_file_path)
@@ -67,19 +67,17 @@ def WIPExport(*args):
     if valid_directory_path:
         while os.path.isfile(full_file_path):
             version_no += 1
-            file_path = name + "_" + "{:03d}".format(version_no) + ".abc"
+            file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
             full_file_path = os.path.join(this_directory_name, file_path)  # Update the full file path
             print("version exists, retrying")
 
         print("WIP export")
         print("Exporting to:", full_file_path)
         
-       # cmds.select(assets)
-        cmds.AbcExport(j="-frameRange 1 120 -root " + assets + " -file " + full_file_path)
-       # else:
-        #    print("No objects selected for export.")
+        cmds.file(rename=full_file_path)
+        cmds.file(save=True, type="mayaBinary")
     else:
-        print("Directory not set or not valid. Please set the directory using setDirectory.")
+        print("Directory not set or not valid.")
 
 
 def publishExport(*args):
@@ -87,30 +85,47 @@ def publishExport(*args):
     global name 
     global WIP_path
     global publish_path
-    latest_published_version = ""
     version_no = 1
-    this_WIP_directory = this_directory_name = WIP_path + "/" + name
-    file_path = name + "_" + "{:03d}".format(version_no) + ".abc"
-    full_file_path = os.path.join(this_directory_name, file_path)  # Construct the full file path
+    dir_no = 1
+    this_directory_name = WIP_path + "/" + name
+    file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
+    full_file_path = os.path.join(this_directory_name, file_path)
+    
+    # Find the latest published version
+    latest_published_version = ""
     while os.path.isfile(full_file_path):
         latest_published_version = full_file_path
         version_no += 1
-        file_path = name + "_" + "{:03d}".format(version_no) + ".abc"
-        full_file_path = os.path.join(this_directory_name, file_path)  # Update the full file path
-    new_file_name = name + "_FINAL.abc"  # Replace with the new file name you want
-    print(publish_path)
-    # Construct the new file path by combining the directory and the new name
-    new_file_path = os.path.join(publish_path, new_file_name)
-    print(new_file_path)
-    print(latest_published_version)
-    try:
-        # Copy the source file to the destination folder and rename it
-        shutil.copy2(latest_published_version, new_file_path)
-        print(f"File copied and renamed to: {new_file_path}")
-    except Exception as e:
-        print(f"Error copying and renaming the file: {str(e)}") 
+        file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
+        full_file_path = os.path.join(this_directory_name, file_path)
+    
+    new_file_name = file_path
+    this_publish_path = os.path.join(publish_path, name)
+    setPiece_path = this_publish_path + "/setPiece"
+    set_path = this_publish_path + "/sets"    
+    layout_path = this_publish_path + "/layout"
+    animation_path = this_publish_path +"/animations"
+    
+    if not os.path.exists(this_publish_path):
+        os.makedirs(this_publish_path)
+        os.makedirs(setPiece_path)
+        os.makedirs(set_path)
+        os.makedirs(layout_path)
+        os.makedirs(animation_path)
         
-
+    cmds.file(latest_published_version, open=True, force=True) ##change to read without having to open??
+    all_sets = cmds.listSets(allSets=True)
+    for set_name in all_sets:
+        if set_name != 'defaultLastHiddenSet':
+            cmds.select(set_name, replace=True)
+            exported_file = set_path + "/" + set_name
+            cmds.file(exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
+            objects_in_set = cmds.listConnections(set_name, type='transform', shapes=True)
+            for object_name in objects_in_set:
+                cmds.select(object_name, replace=True)
+                exported_file = setPiece_path + "/" + object_name
+                cmds.file(exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
+        
 def assetPublishing():
     global directoryField 
     window_name = 'assetPublishing'
