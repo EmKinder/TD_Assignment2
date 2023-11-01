c07ff9688604a1785c409b97239abb100037bf02
EmKinder <89366821+EmKinder@users.noreply.github.com>
2023-11-01 16:29:17 +1100

    WIP and publishing with one ABC file added

--- Source/AssetPublishing.py
+++ Source/AssetPublishing.py
@@ -1,5 +1,17 @@
 import maya.cmds as cmds
 import os
+import platform
+import shutil
+
+
+current_platform = platform.system()
+
+if current_platform == 'Windows':
+    print("user using windows")
+elif current_platform == 'Linux':
+    print("user using linux")
+else:
+    print("user using something else")
 
 directoryField = None
 directory = ""
@@ -12,6 +24,8 @@ def setDirectory(*args):
     global directoryField 
     global directory
     global valid_directory_path
+    global WIP_path
+    global publish_path
     selected_directory = cmds.fileDialog2(dialogStyle=2, fileMode=3)
     if selected_directory:
         directory = selected_directory[0]
@@ -29,23 +43,73 @@ def setDirectory(*args):
 def updateName(*args):
     global name
     name = cmds.textField('nameText', query=True, text=True)
-    print("Sequence Name entered:", sequence_name)
+    print("Sequence Name entered:", name)
+    
 
 def WIPExport(*args):
     updateName()
-    global name 
+    global name
     global WIP_path
     global valid_directory_path
-    models = cmds.ls(geometry=True)
-    set_pieces = cmds.ls(type='objectSet')
-    animations = cmds.keyframe(q=True)
-    lighting = cmds.ls(type='light')
-    if(valid_directory_path):
-        cmds.file(file_path, force=True, typ="mayaBinary", exportSelected=True)
-    print("WIP export")
+    assets = cmds.ls(type="transform", selection=True)[0]
+    version_no = 1
+    this_directory_name = WIP_path + "/" + name
+    print("WIP_path:", WIP_path)
+    print("this_directory_name:", this_directory_name)
+
+    if not os.path.exists(this_directory_name):
+        os.makedirs(this_directory_name)
+    file_path = name + "_" + "{:03d}".format(version_no) + ".abc"
+    full_file_path = os.path.join(this_directory_name, file_path)  # Construct the full file path
+
+    print("full_file_path:", full_file_path)
+
+    if valid_directory_path:
+        while os.path.isfile(full_file_path):
+            version_no += 1
+            file_path = name + "_" + "{:03d}".format(version_no) + ".abc"
+            full_file_path = os.path.join(this_directory_name, file_path)  # Update the full file path
+            print("version exists, retrying")
+
+        print("WIP export")
+        print("Exporting to:", full_file_path)
+        
+       # cmds.select(assets)
+        cmds.AbcExport(j="-frameRange 1 120 -root " + assets + " -file " + full_file_path)
+       # else:
+        #    print("No objects selected for export.")
+    else:
+        print("Directory not set or not valid. Please set the directory using setDirectory.")
+
 
 def publishExport(*args):
-    print("Publish export")
+    updateName()
+    global name 
+    global WIP_path
+    global publish_path
+    latest_published_version = ""
+    version_no = 1
+    this_WIP_directory = this_directory_name = WIP_path + "/" + name
+    file_path = name + "_" + "{:03d}".format(version_no) + ".abc"
+    full_file_path = os.path.join(this_directory_name, file_path)  # Construct the full file path
+    while os.path.isfile(full_file_path):
+        latest_published_version = full_file_path
+        version_no += 1
+        file_path = name + "_" + "{:03d}".format(version_no) + ".abc"
+        full_file_path = os.path.join(this_directory_name, file_path)  # Update the full file path
+    new_file_name = name + "_FINAL.abc"  # Replace with the new file name you want
+    print(publish_path)
+    # Construct the new file path by combining the directory and the new name
+    new_file_path = os.path.join(publish_path, new_file_name)
+    print(new_file_path)
+    print(latest_published_version)
+    try:
+        # Copy the source file to the destination folder and rename it
+        shutil.copy2(latest_published_version, new_file_path)
+        print(f"File copied and renamed to: {new_file_path}")
+    except Exception as e:
+        print(f"Error copying and renaming the file: {str(e)}") 
+        
 
 def assetPublishing():
     global directoryField 
