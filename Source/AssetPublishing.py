e7ba58e05a0613e822a522ca3ca9618c54493cf8
EmKinder <emily.l.kinder@student.uts.edu.au>
2023-11-01 23:31:56 +1100

    check if keyframes exist before creating cache folders/exporting

--- Source/AssetPublishing.py
+++ Source/AssetPublishing.py
@@ -1,17 +1,8 @@
 import maya.cmds as cmds
 import os
-import platform
 import shutil
 import re
-
-current_platform = platform.system()
-
-if current_platform == 'Windows':
-    print("user using windows")
-elif current_platform == 'Linux':
-    print("user using linux")
-else:
-    print("user using something else")
+import maya.mel as mel
 
 directoryField = None
 directory = ""
@@ -93,50 +84,55 @@ def publishExport(*args):
     
     # Find the latest published version
     latest_published_version = ""
-    while os.path.isfile(full_file_path):
-        latest_published_version = full_file_path
-        version_no += 1
-        file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
-        full_file_path = os.path.join(this_directory_name, file_path)
-    
-    new_file_name = file_path
-    this_publish_path = os.path.join(publish_path, name)
-    setPiece_path = this_publish_path + "/setPiece"
-    set_path = this_publish_path + "/sets"    
-    layout_path = this_publish_path + "/layout"
-    animation_path = this_publish_path +"/animations"
-    
-    if not os.path.exists(this_publish_path):
-        os.makedirs(this_publish_path)
-        os.makedirs(setPiece_path)
-        os.makedirs(set_path)
-        os.makedirs(layout_path)
-        os.makedirs(animation_path)
+    if valid_directory_path:
+        while os.path.isfile(full_file_path):
+            latest_published_version = full_file_path
+            version_no += 1
+            file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
+            full_file_path = os.path.join(this_directory_name, file_path)
         
-    cmds.file(latest_published_version, open=True, force=True)
-    all_sets = cmds.listSets(allSets=True)
-    for set_name in all_sets:
-        if set_name != 'defaultLastHiddenSet' and set_name != 'defaultHideFaceDataSet' and set_name != 'defaultCreaseDataSet':
-            cmds.select(set_name, replace=True)
-            exported_file = set_path + "/" + set_name
-            cmds.file(exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
-            objects_in_set = cmds.listConnections(set_name, type='transform', shapes=True)
-            for object_name in objects_in_set:
-                cmds.select(object_name, replace=True)
-                exported_file = os.path.join(setPiece_path, object_name + ".mb")
+        new_file_name = file_path
+        this_publish_path = os.path.join(publish_path, name)
+        setPiece_path = this_publish_path + "/setPiece"
+        set_path = this_publish_path + "/sets"    
+        layout_path = this_publish_path + "/layout"
+        animation_path = this_publish_path +"/animations"
+        
+        if not os.path.exists(this_publish_path):
+            os.makedirs(this_publish_path)
+            os.makedirs(setPiece_path)
+            os.makedirs(set_path)
+            os.makedirs(layout_path)
+            os.makedirs(animation_path)
+            
+        cmds.file(latest_published_version, open=True, force=True)
+        all_sets = cmds.listSets(allSets=True)
+        for set_name in all_sets:
+            if set_name != 'defaultLastHiddenSet' and set_name != 'defaultHideFaceDataSet' and set_name != 'defaultCreaseDataSet':
+                cmds.select(set_name, replace=True)
+                exported_file = set_path + "/" + set_name
                 cmds.file(exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
-                FBX_file_path = os.path.join(animation_path, object_name, "fbx")
-                ABC_file_path = os.path.join(animation_path, object_name, "alembic")
-                if not os.path.exists(FBX_file_path):
-                    os.makedirs(FBX_file_path)
-                if not os.path.exists(ABC_file_path):
-                    os.makedirs(ABC_file_path)
-                alembic_file = os.path.join(ABC_file_path, object_name + ".abc")
-                fbx_export_file = os.path.join(FBX_file_path, object_name + ".fbx")
-                print(fbx_export_file)
-               # cmds.file(fbx_export_file, force=True, options="v=0;", type="FBX export", pr=False)
-                cmds.AbcExport(j="-frameRange 1 120 -root " + object_name + " -file " + alembic_file)
-        
+                objects_in_set = cmds.listConnections(set_name, type='transform', shapes=True)
+                for object_name in objects_in_set:
+                    cmds.select(object_name, replace=True)
+                    exported_file = os.path.join(setPiece_path, object_name + ".mb")
+                    cmds.file(exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
+                    keyframes = cmds.keyframe(object_name, query=True, timeChange=True)
+                    if keyframes:
+                        FBX_file_path = os.path.join(animation_path, object_name, "fbx")
+                        ABC_file_path = os.path.join(animation_path, object_name, "alembic")
+                        if not os.path.exists(FBX_file_path):
+                            os.makedirs(FBX_file_path)
+                        if not os.path.exists(ABC_file_path):
+                            os.makedirs(ABC_file_path)
+                        alembic_file = os.path.join(ABC_file_path, object_name + ".abc")
+                        fbx_export_path = os.path.join(FBX_file_path, object_name + ".fbx")
+                        #mel_command = 'FBXExport -f "{0}" -s -typ "FBX export" -pr -ea 1 -es false -et 1 -en false -fme 0 -fmi 1 -em false -v fbxm'.format(fbx_export_path)
+                        #mel.eval(mel_command)
+                        cmds.AbcExport(j="-frameRange 1 120 -root " + object_name + " -file " + alembic_file)
+    else:
+        print("Directory not set or not valid.")
+                
 def assetPublishing():
     global directoryField 
     window_name = 'assetPublishing'
