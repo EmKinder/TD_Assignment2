10affccf3f437f3d0e1d6015489b6b7c85762964
EmKinder <emily.l.kinder@student.uts.edu.au>
2023-11-02 19:34:50 +1100

    UI adjustments, new folders for each setpiece

--- Source/AssetPublishing.py
+++ Source/AssetPublishing.py
@@ -12,8 +12,6 @@ publish_path = ""
 valid_directory_path = False
 name = ""
 department_name = "Modelling"
-#options = "-s"  # Example FBX export options (modify as needed)
-
 
 def setDirectory(*args):
     global directoryField 
@@ -24,7 +22,7 @@ def setDirectory(*args):
     selected_directory = cmds.fileDialog2(dialogStyle=2, fileMode=3)
     if selected_directory:
         directory = selected_directory[0]
-        if not os.path.exists(directory + "/WIP"):
+        if not os.path.exists(directory + "/WIP") or os.path.exists(directory + "/Published"):
             customPopup("Directory not valid (missing either WIP or Published folder")
             cmds.textField(directoryField, edit=True, text="")
             valid_directory_path = False
@@ -49,44 +47,48 @@ def WIPExport(*args):
     global valid_directory_path
     global department_name
     print("Department: " + department_name)
-    #assets = cmds.ls(type="transform", selection=True)[0]
     version_no = 1
-    this_directory_name = WIP_path + "/" + name
-    sequence_directory = this_directory_name + "/Sequence"
-    assets_directory = this_directory_name + "/Assets"
-    print("WIP_path:", WIP_path)
-    print("this_directory_name:", this_directory_name)
-
-    if not os.path.exists(this_directory_name):
-        os.makedirs(this_directory_name)
-        os.makedirs(sequence_directory)
-        os.makedirs(assets_directory)
-    file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
-    if department_name == "Modelling":
-        full_file_path = os.path.join(assets_directory, file_path)
+    if not name == "":
+        this_directory_name = WIP_path + "/" + name
+        sequence_directory = this_directory_name + "/Sequence"
+        assets_directory = this_directory_name + "/Assets"
+        print("WIP_path:", WIP_path)
+        print("this_directory_name:", this_directory_name)
+    
+        if not os.path.exists(this_directory_name):
+            os.makedirs(this_directory_name)
+            os.makedirs(sequence_directory)
+            os.makedirs(assets_directory)
+        file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
+        if department_name == "Modelling":
+            full_file_path = os.path.join(assets_directory, file_path)
+        else:
+            full_file_path = os.path.join(sequence_directory, file_path)
+    
+        print("full_file_path:", full_file_path)
+    
+        if valid_directory_path:
+            while os.path.isfile(full_file_path):
+                version_no += 1
+                file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
+                if department_name == "Modelling":
+                    full_file_path = os.path.join(assets_directory, file_path)
+                else:
+                    full_file_path = os.path.join(sequence_directory, file_path)
+                print("version exists, retrying")
+    
+            print("WIP export")
+            print("Exporting to:", full_file_path)
+            
+            cmds.file(rename=full_file_path)
+            cmds.file(save=True, type="mayaBinary")
+        else:
+            print("Directory not set or not valid.")
+            customPopup("Directory not set or not valid.")
+            return
+        customPopup("Publish Success!")
     else:
-        full_file_path = os.path.join(sequence_directory, file_path)
-
-    print("full_file_path:", full_file_path)
-
-    if valid_directory_path:
-        while os.path.isfile(full_file_path):
-            version_no += 1
-            file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
-            if department_name == "Modelling":
-                full_file_path = os.path.join(assets_directory, file_path)
-            else:
-                full_file_path = os.path.join(sequence_directory, file_path)
-            print("version exists, retrying")
-
-        print("WIP export")
-        print("Exporting to:", full_file_path)
-        
-        cmds.file(rename=full_file_path)
-        cmds.file(save=True, type="mayaBinary")
-    else:
-        print("Directory not set or not valid.")
-
+        customPopup("No name specified")
 
 def publishExport(*args):
     updateName()
@@ -96,92 +98,108 @@ def publishExport(*args):
     global directory_name
     version_no = 1
     dir_no = 1
-    if department_name == "Modelling":
-        this_directory_name = WIP_path + "/" + name + "/Assets"
-    else:
-        this_directory_name = WIP_path + "/" + name + "/Sequence"
-    file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
-    full_file_path = os.path.join(this_directory_name, file_path)
-    
-    # Find the latest published version
-    latest_published_version = ""
-    if valid_directory_path:
-        while os.path.isfile(full_file_path):
-            latest_published_version = full_file_path
-            version_no += 1
-            file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
-            full_file_path = os.path.join(this_directory_name, file_path)
+    cam_no = 1
+    set_no = 1
+    setpiece_no = 1
+    if not name == "":
+        if department_name == "Modelling":
+            this_directory_name = WIP_path + "/" + name + "/Assets"
+        else:
+            this_directory_name = WIP_path + "/" + name + "/Sequence"
+        file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
+        full_file_path = os.path.join(this_directory_name, file_path)
         
-        new_file_name = file_path
-        this_publish_path = os.path.join(publish_path, name)
-        setPiece_path = this_publish_path + "/setPiece"
-        set_path = this_publish_path + "/sets"    
-        layout_path = this_publish_path + "/layout"
-        animation_path = this_publish_path +"/animations"
-        layout_source_path = layout_path + "/source"
-        layout_cache_path = layout_path + "/cache"
-        
-        if not os.path.exists(this_publish_path):
-            os.makedirs(this_publish_path)
-            os.makedirs(setPiece_path)
-            os.makedirs(set_path)
-            os.makedirs(layout_path)
-            os.makedirs(animation_path)
-            os.makedirs(layout_cache_path)
-            os.makedirs(layout_source_path)
+        latest_published_version = ""
+        if valid_directory_path:
+            while os.path.isfile(full_file_path):
+                latest_published_version = full_file_path
+                version_no += 1
+                file_path = name + "_" + "{:03d}".format(version_no) + ".mb"
+                full_file_path = os.path.join(this_directory_name, file_path)
             
-        cmds.file(latest_published_version, open=True, force=True)
-        default_cameras = ["frontShape", "perspShape", "sideShape", "topShape"]
-        all_cameras = cmds.ls(type='camera')
-        cameras = [cam for cam in all_cameras if cam not in default_cameras]
-        for camera_name in cameras:
-            print(camera_name)
-            if not camera_name:
-                print("no camera selected")
-            else:
-                cmds.select(camera_name, replace=True)
-                source_exported_file = os.path.join(layout_source_path , camera_name + ".mb")
-                cache_exported_file = os.path.join(layout_cache_path , camera_name + ".abc")
-                cmds.file(source_exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
-                cmds.AbcExport(j="-frameRange 1 120 -root " + camera_name + " -file " + cache_exported_file)
-        print("Exporting Cameras Done")
-        deault_sets = ["defaultLastHiddenSet", "defaultHideFaceDataSet", "defaultCreaseDataSet", "defaultObjectSet", "defaultLightSet", "internal_standInSE", "internal_soloSE", "initialParticleSE", "initialShadingGroup"]     
-        all_sets = cmds.listSets(allSets=True)
-        set_list = [set for set in all_sets if set not in deault_sets]
-        for set_name in set_list:
-            print(set_name)
-            if not set_name:
-                print("no set selected")
-            else:
-                cmds.select(set_name, replace=True)
-                exported_file = set_path + "/" + set_name
-                cmds.file(exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
-                objects_in_set = cmds.listConnections(set_name, type='transform', shapes=True)
-                print("Exporting Sets Done")
-                for object_name in objects_in_set:
-                    cmds.select(object_name, replace=True)
-                    exported_file = os.path.join(setPiece_path, object_name + ".mb")
+            new_file_name = file_path
+            this_publish_path = os.path.join(publish_path, name)
+            setPiece_path = this_publish_path + "/setPiece"
+            set_path = this_publish_path + "/sets"    
+            layout_path = this_publish_path + "/layout"
+            animation_path = this_publish_path +"/animations"
+            layout_source_path = layout_path + "/source"
+            layout_cache_path = layout_path + "/cache"
+            
+            if not os.path.exists(this_publish_path):
+                os.makedirs(this_publish_path)
+                os.makedirs(setPiece_path)
+                os.makedirs(set_path)
+                os.makedirs(layout_path)
+                os.makedirs(animation_path)
+                os.makedirs(layout_cache_path)
+                os.makedirs(layout_source_path)
+                
+            cmds.file(latest_published_version, open=True, force=True)
+            default_cameras = ["frontShape", "perspShape", "sideShape", "topShape"]
+            all_cameras = cmds.ls(type='camera')
+            cameras = [cam for cam in all_cameras if cam not in default_cameras]
+            for camera_name in cameras:
+                print(camera_name)
+                if not camera_name:
+                    print("no camera selected")
+                else:
+                    cmds.select(camera_name, replace=True)
+                    source_exported_file = layout_source_path + "/" + camera_name + "_" + "{:03d}".format(cam_no) + ".mb"
+                    cache_exported_file = layout_cache_path + "/" + camera_name + "_" + "{:03d}".format(cam_no) + ".abc"
+                    while os.path.isfile(source_exported_file):
+                        cam_no += 1
+                        source_exported_file = layout_source_path + "/" + camera_name + "_" + "{:03d}".format(cam_no) + ".mb"
+                        cache_exported_file = layout_cache_path + "/" + camera_name + "_" + "{:03d}".format(cam_no) + ".abc"
+                    cmds.file(source_exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
+                    cmds.AbcExport(j="-frameRange 1 120 -root " + camera_name + " -file " + cache_exported_file)
+            print("Exporting Cameras Done")
+            deault_sets = ["defaultLastHiddenSet", "defaultHideFaceDataSet", "defaultCreaseDataSet", "defaultObjectSet", "defaultLightSet", "internal_standInSE", "internal_soloSE", "initialParticleSE", "initialShadingGroup"]     
+            all_sets = cmds.listSets(allSets=True)
+            set_list = [set for set in all_sets if set not in deault_sets]
+            for set_name in set_list:
+                print(set_name)
+                if not set_name:
+                    print("no set selected")
+                else:
+                    cmds.select(set_name, replace=True)
+                    exported_file = set_path + "/" + set_name + "_" + "{:03d}".format(set_no) + ".mb"
+                    while os.path.isfile(exported_file):
+                        set_no += 1
+                        exported_file = set_path + "/" + set_name + "_" + "{:03d}".format(set_no) + ".mb"                    
                     cmds.file(exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
-                    keyframes = cmds.keyframe(object_name, query=True, timeChange=True)
-                    if keyframes:
-                        FBX_file_path = animation_path +"/" + object_name +"/fbx"
-                        ABC_file_path = os.path.join(animation_path, object_name, "alembic")
-                        if not os.path.exists(FBX_file_path):
-                            os.makedirs(FBX_file_path)
-                        if not os.path.exists(ABC_file_path):
-                            os.makedirs(ABC_file_path)
-                        alembic_file = os.path.join(ABC_file_path, object_name + ".abc")
-                        fbx_export_path = FBX_file_path + "/" + object_name
-                        print(fbx_export_path)
-                        #cmds.FBXExport(f=fbx_export_path, s=True)
-                        cmds.file(fbx_export_path, force=True, type="FBX export", pr=True, es=True )
-                        cmds.AbcExport(j="-frameRange 1 120 -root " + object_name + " -file " + alembic_file)
+                    objects_in_set = cmds.listConnections(set_name, type='transform', shapes=True)
+                    print("Exporting Sets Done")
+                    for object_name in objects_in_set:
+                        cmds.select(object_name, replace=True)
+                        set_export_path = setPiece_path + "/" + object_name
+                        exported_file = set_export_path + "/" + object_name + "_" + "{:03d}".format(setpiece_no) + ".mb"
+                        while os.path.isfile(exported_file):
+                            setpiece_no += 1
+                            exported_file = set_export_path + "/" + object_name + "_" + "{:03d}".format(setpiece_no) + ".mb"
+                        cmds.file(exported_file, exportSelected=True, type='mayaBinary', preserveReferences=False)
+                        keyframes = cmds.keyframe(object_name, query=True, timeChange=True)
+                        if keyframes:
+                            FBX_file_path = animation_path +"/" + object_name +"/fbx"
+                            ABC_file_path = os.path.join(animation_path, object_name, "alembic")
+                            if not os.path.exists(FBX_file_path):
+                                os.makedirs(FBX_file_path)
+                            if not os.path.exists(ABC_file_path):
+                                os.makedirs(ABC_file_path)
+                            alembic_file = os.path.join(ABC_file_path, object_name + "_" + "{:03d}".format(setpiece_no) + ".abc")
+                            fbx_export_path = FBX_file_path + "/" + object_name + "_" + "{:03d}".format(setpiece_no)
+                            print(fbx_export_path)
+                            cmds.file(fbx_export_path, force=True, type="FBX export", pr=True, es=True )
+                            cmds.AbcExport(j="-frameRange 1 120 -root " + object_name + " -file " + alembic_file)
+        else:
+            print("Directory not set or not valid.")
+            customPopup("Directory not set or not valid.")
+            return
+        customPopup("Publish Success!")
     else:
-        print("Directory not set or not valid.")
-        
+        customPopup("No name specified")        
 def menu_item_selected(selection):
     global department_name
-    cmds.confirmDialog(title='Menu Item Selected', message='You selected: ' + selection, button='OK')
     department_name = selection
     print("Department Changed To: " + department_name)
                 
@@ -194,18 +212,22 @@ def assetPublishing():
     cmds.window(window_name, title="Asset Publishing", widthHeight=(500, 200))
     
     main_layout = cmds.columnLayout(adjustableColumn=True)
-    
+    cmds.separator(h=10, style='none')
     directoryField = cmds.textField('directoryField', w=300, placeholderText="Select Directory Path")
-
+    cmds.separator(h=5, style='none')
     cmds.button(label="Set Base Directory", command=setDirectory)
     
     cmds.separator(h=10, style='none')
+    cmds.separator(h=5, style='double')
+    cmds.separator(h=5, style='none')
     
     cmds.text('Name', align="center", font="boldLabelFont")
     cmds.separator(h=5, style='none')
     text_field = cmds.textField('nameText', placeholderText="Name")
     
     cmds.separator(h=10, style='none')
+    cmds.separator(h=5, style='double')
+    cmds.separator(h=5, style='none')
     
     menu_label = 'Department'
     menu_items = ['Modelling', 'Layout', 'Animation', 'Lighting']
@@ -215,11 +237,15 @@ def assetPublishing():
     for item in menu_items:
         cmds.menuItem(label=item)
         
-    cmds.separator(h=10, style='none')     
+    cmds.separator(h=10, style='none')
+    cmds.separator(h=5, style='double')
+    cmds.separator(h=5, style='none')     
            
     cmds.button(label="WIP", command=WIPExport)
     cmds.separator(h=5, style='none')
     cmds.button(label="Publish", command=publishExport)  
+    
+    cmds.separator(h=10, style='none')
     
     
     cmds.showWindow('assetPublishing')
@@ -229,7 +255,7 @@ def customPopup(message):
     if cmds.window("customPopupWindow", exists=True):
         cmds.deleteUI("customPopupWindow")
 
-    customPopupWindow = cmds.window("customPopupWindow", title="Custom Popup", widthHeight=(200, 100))
+    customPopupWindow = cmds.window("customPopupWindow", title="System Message", widthHeight=(200, 50))
     mainLayout = cmds.columnLayout(adjustableColumn=True, parent=customPopupWindow)
     cmds.text(label=message)
     cmds.button(label="Close", command=lambda *args: cmds.deleteUI(customPopupWindow))
